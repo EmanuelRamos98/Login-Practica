4ea063a4573d93116b1c24a017aacdfd58fc4363
EmanuelRamos98 <82544588+EmanuelRamos98@users.noreply.github.com>
2024-06-19 16:01:26 -0300

    segundo Commit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+const validarUsername = (texto)=> texto.lenth >=4 && texto != texto.toLowerCase()
+const validarPassword = (texto)=> texto.lenth >=6 && texto != texto.toLowerCase() && texto.incluedes('#')
+
 const formularioHTML = document.getElementById('formulario')
 
-formularioHTML.addEventListener("submit",(evento)=>{
+const handleSubmit = (evento)=>{
     evento.preventDefault()
+    const formulario = evento.target
+    const username = formulario.username.value
+    const password = formulario.password.value
     
-})
+    let valido = true
+
+    if(!validarUsername(username)){
+        const errorTextHTML = document.querySelector('.error-text-username')
+        errorTextHTML.innerText = 'El username tiene que tener al menos 4 caracteres y al menos una mayuscula'
+    }
+    if(!validarPassword(password)){
+        const errorTextHTML = document.querySelector('.error-text-password')
+        errorTextHTML.innerText = 'El password debe contener al menos 6 caracteres, una mayuscula e incluir (#)'
+    }
+}
+
+formularioHTML.addEventListener('submit', handleSubmit)
+
