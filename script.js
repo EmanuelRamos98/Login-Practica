dfd14ac8713a7c395b66f694a98284c39ff2104d
EmanuelRamos98 <82544588+EmanuelRamos98@users.noreply.github.com>
2024-06-21 18:44:31 -0300

    Tercer commit

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-const validarUsername = (texto)=> texto.lenth >=4 && texto != texto.toLowerCase()
-const validarPassword = (texto)=> texto.lenth >=6 && texto != texto.toLowerCase() && texto.incluedes('#')
+const validarMayuscula =(texto) =>{
+    let textoMinuscula = texto.toLowerCase()
+    return textoMinuscula !== texto
+}
+
+const VALIDACIONES = {
+    username:{
+        validacion: (texto)=> texto.length >=4 && validarMayuscula(texto),
+        errorText: 'It must be at least 4 characters and an uppercase'
+    } ,
+    password:{
+        validacion: (texto)=> texto.length >=6 && validarMayuscula(texto),
+        errorText: 'It must be at least 6 characters and an uppercase'
+    }
+}
+
+const obtenerValoresFormulario = (formulario)=>{
+    const inputs = ['username', 'password']
+    const inputsInfo = {}
+    for(let input of inputs){
+        const spanErrorHTML = formulario.querySelector('.error-text-'+ input)
+        const valor = formulario[input].value
+        inputsInfo[input] = {
+            errorHTML: spanErrorHTML,
+            validacion: VALIDACIONES[input].validacion,
+            errorText: VALIDACIONES[input].errorText,
+            valor: valor
+        }
+    }
+    return inputsInfo
+}
 
 const formularioHTML = document.getElementById('formulario')
 
 const handleSubmit = (evento)=>{
     evento.preventDefault()
     const formulario = evento.target
-    const username = formulario.username.value
-    const password = formulario.password.value
-    
-    let valido = true
-
-    if(!validarUsername(username)){
-        const errorTextHTML = document.querySelector('.error-text-username')
-        errorTextHTML.innerText = 'El username tiene que tener al menos 4 caracteres y al menos una mayuscula'
-    }
-    if(!validarPassword(password)){
-        const errorTextHTML = document.querySelector('.error-text-password')
-        errorTextHTML.innerText = 'El password debe contener al menos 6 caracteres, una mayuscula e incluir (#)'
+    const valoresFormulario = obtenerValoresFormulario(formulario)
+    console.log(valoresFormulario);
+    for(const propiedad in valoresFormulario){
+        const objetoInputFormulario = valoresFormulario[propiedad]
+        if(!objetoInputFormulario.validacion(objetoInputFormulario.valor)){
+            objetoInputFormulario.errorHTML.innerText = objetoInputFormulario.errorText
+        }
+        else{
+            objetoInputFormulario.errorHTML.innerText = ''
+        }
     }
 }
 
